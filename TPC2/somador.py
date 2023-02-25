2d9a155a455c7c99fabed6668aa5a4c0df02311b
joaofaria19 <joaofaria.dias04@gmail.com>
2023-02-25 18:47:19 +0000

    tpc corrigido

--- TPC2/somador.py
+++ TPC2/somador.py
@@ -1,43 +1,60 @@
 import sys
 
-def list_words(lines):
-    palavras=[]
-    soma_flag=False
-    for line in lines:
-        splited = line.strip().split()
-        palavras.extend(splited)
+def list_to_digit(lista):
+    l = len(lista)
+    n=1
+    num=0
+    while l>0:
+        num+=int(lista[l-1])*n
+        n *= 10
+        l-=1
+    return num
+
+def extrair(texto):
+    palavras = []
+    i = 0
+    while i < len(texto):
+        if texto[i] == '=':
+            palavras.append('=')
+            i += 1
+        elif texto[i] == 'o' and texto[i+1] == 'n':
+            palavras.append('on')
+            i += 2
+        elif texto[i] == 'o' and texto[i+1] == 'f' and texto[i+2] == 'f':
+            palavras.append('off')
+            i += 3
+        elif texto[i].isdigit():
+            inicio = i
+            while i < len(texto) and texto[i].isdigit():
+                i += 1
+            palavras.append(list_to_digit(texto[inicio:i]))
+        else:    
+            i += 1
+    print(palavras)
     return palavras
 
-def soma_array_numbers(array):
-    total = 0
-    for i in array:
-        total += float(i)
-    return total
 
-def run(words):
-    soma_flag = False
-    numbers=[]
 
-    for word in words:
-        if(soma_flag==True):
-            if(word.isdigit()):
-                numbers.append(word)
-        if('=' in word) :
-            total = soma_array_numbers(numbers)
-            print("\n(TOTAL): "+ str(total))
-        if("on" in word.lower()):
-            soma_flag=True
-        elif("off" in word.lower()):
-            soma_flag=False
-    
+def run(texto):
+    on_stack = []
+    soma = 0
+    for word in texto:
+        if word == 'on':
+            on_stack.append('on')
+        elif word =='off':
+            on_stack.pop()
+        elif word == '=':
+            print(">> "+ str(soma))
+        elif len(on_stack) > 0:
+            soma += (word)
 
 def main():
     lines = []
     print("<< Insira o texto em seguida e prima 'Ctrl+D' para ver o total >>\n")
     for linha in sys.stdin:
-        lines.append(linha)
-    words = list_words(lines)
+        lines.extend(linha)
+    words = extrair(lines)
     run(words)
-    
+
 if __name__ == "__main__":
     main()
