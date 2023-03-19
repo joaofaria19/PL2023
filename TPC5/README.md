0e593e861845e3b4676bc0e4a4281e18a4f420a9
joaofaria19 <joaofaria.dias04@gmail.com>
2023-03-19 21:26:08 +0000

    TPC 5 feito e readme atualizado

--- TPC5/README.md
+++ TPC5/README.md
@@ -1,3 +1,16 @@
-# PL2023
-Processamento de Linguagens-2023
-TPC5
+# PL2023 - TPC5
+
+O trabalho para casa 5, da unidade curricular de Processamento de linguagens consiste na elaboração de uma máquina de estados que modela a interação de um utilizador com um telefone numa cabine pública.
+
+O telefone reage aos comandos seguintes:
+
+- LEVANTAR - levantar o auscultador, marca o início duma interacção;
+- POUSAR - pousar o auscultador, fim da interacção, deverá ser indicado o montante a ser devolvido;
+- MOEDA <lista de valores> - inserção de moedas (só deverá aceitar moedas válidas, para valores inválidos deverá ser gerada uma mensagem de erro): lista de valores = num, num, ..., num;
+- T=numero - disca o número ( o número deve ter 9 dígitos excepto se for iniciado por "00"); as diferentes chamadas deverão ser tratadas da seguinte maneira:
+    - para números iniciados por "601" ou "641" a chamada é "bloqueada";
+    - para chamadas internacionais (iniciadas por "00") o utilizador tem que ter um saldo igual ou superior a 1,5 euros, caso contrário deverá ser avisado que o saldo é -insuficiente e a máquina volta ao estado anterior; a chamada se for realizada tem um custo de 1,5 euros;
+    - para chamadas nacionais (iniciadas por "2") o saldo mínimo e custo de chamada é de 25 cêntimos;
+    - para chamadas verdes (iniciadas por "800") o custo é 0;
+    - para chamadas azuis (iniciadas por "808") o custo é de 10 cêntimos.
+- ABORTAR - interromper a interacção; a máquina devolve as moedas.
--- TPC5/cabine.py
+++ TPC5/cabine.py
@@ -2,102 +2,134 @@ import re
 import ply.lex as lex
 import sys
 
-# Coins list
-moedas_validas = ['5c','10c','20c','50c','1e','2e']
+moedas_exp = re.compile(r'^MOEDA\s+(?P<moedas>(\d+(c|e)(,\s*|(?=;)|(?=.))*)*)')
 
-saldo = 0
+numero_exp = re.compile(r'^T=\s*(?P<numero>\d{9,})$')
 
-stack  = []
-
-moedas_exp = re.compile(r'^MOEDA\s+(?P<moedas>(\d+(c|e)(,\s*|(?=;)|(?=.)))*)')
-
-numero_exp = re.compile(r'^T=(?P<numero>\d{9,})$')
-
+"""
+    Função responsável por pegar num número correspondente a uma moeda ou à soma 
+de várias moedas, e transformar esse número no respetivo formato, uma string correspondente 
+ao valor introduzido.
+"""
 def intToStringCoin(moeda):
-    f_moeda = int(moeda/100)
-    s_moeda = moeda - 100*f_moeda
+    moeda_euro = int(moeda/100)
+    moeda_cent = moeda - 100*moeda_euro
     moeda_string = ''
-    if f_moeda > 0:
-        moeda_string = str(f_moeda)+'e'+str(s_moeda)+'c'
+    if moeda_euro>0 and moeda_cent <= 0:
+        moeda_string = str(moeda_euro)+'e'
+    elif moeda_euro > 0:
+        moeda_string = str(moeda_euro)+'e'+str(moeda_cent)+'c'
     else:
-        moeda_string = str(s_moeda)+'c'
+        moeda_string = str(moeda_cent)+'c'
     return moeda_string
 
+"""
+    Função responsável por verificar as várias moedas introduzidas, recebe portanto a lista de moedas introduzidas. 
+Se a moeda for válida esta é adicionada ao valor total de moedas, se a moeda for inválida 
+é acrescentada à lista de moedas inválidas. No final é retornado um tuplo contendo o valor total de moedas e 
+a lista de moedas inválidas (que será convertida numa única string), respetivamente.
+"""
 def moeda_handler(lista_moedas):
+    moedas_validas = ['5c','10c','20c','50c','1e','2e']
     moedas_total = 0
     moedas_invalidas = []
+    
     for moeda in lista_moedas:
         if moeda not in moedas_validas:
             moedas_invalidas.append(moeda)
         else:
             if moeda == '1e' or moeda == '2e':
                 moeda = moeda[:-1]
-                moeda*100
+                moeda = int(moeda) * 100
             else: 
                 moeda = moeda[:-1]
             moedas_total += int(moeda)
     
     moeda_invalidas_string=''
-    if len(moedas_invalidas) > 0:
 
+    if len(moedas_invalidas) > 0:
         for moeda in moedas_invalidas:
-            moeda_invalidas_string += moeda +", "
+            moeda_invalidas_string += moeda+", "
 
     return (moedas_total,moeda_invalidas_string)
 
+"""
+    Função run trata de todo o processo para a realização de uma chamada.
+Para cada opção disponível faz a devida verificação se a mesma pode ocorrer ou não.
+Faz uso de uma stack que permite guardar o estado em que o programa se encontra.
+"""
+def run():
+    saldo = 0
+    stack  = []
+    for linha in sys.stdin:
+        if linha.strip().upper() == 'LEVANTAR' and len(stack) == 0:
+            print("maq: 'Introduza moedas'")
+            stack.append(linha.strip())
 
-for linha in sys.stdin:
-    if linha.strip().upper() == 'LEVANTAR' and len(stack) == 0:
-        print("maq: 'Introduza moedas'")
-        stack.append(linha.strip())
-        
-    elif stack[-1].upper() == 'LEVANTAR'and moedas_exp.match(linha.strip()):
-        moedas = moedas_exp.match(linha).group('moedas').split(',')
-        (total, invalidas) = moeda_handler(moedas)
-        total_string = intToStringCoin(total)
-        saldo += total
-        if len(invalidas) == 0:
-            print('maq: "saldo = '+ total_string +'"')
+        elif len(stack) > 0  and moedas_exp.match(linha.strip()):
+            if stack[-1].upper() == 'LEVANTAR' or stack[-1].upper() == 'MOEDA':
+                moedas = moedas_exp.match(linha).group('moedas').split(',')
+                (total, invalidas) = moeda_handler(moedas)
+                saldo += total
+                total_string = intToStringCoin(saldo)
+                if len(invalidas) == 0:
+                    print('maq: "saldo = '+ total_string +'"')
+                else:
+                    print('maq: "'+ str(invalidas) +' moeda inválida, saldo = '+total_string+'"')
+                stack.append('MOEDA')
+
+        elif len(stack)>0 and numero_exp.match(linha.strip()):
+            if stack[-1].upper() == 'MOEDA': 
+                numero = numero_exp.match(linha).group('numero')
+                if len(numero) >= 9 :
+                    if numero[:3] == '601' or numero[3:] == '641':
+                        print('maq: "Esse número não é permitido neste telefone. Queira disque um novo número"')
+                    elif numero[:2] == '00':
+                        if saldo>150:
+                            saldo-=150
+                            stack.append('T')
+                            print('maq: "saldo %s"' % intToStringCoin(saldo))
+                        else:    
+                            print('maq: "Saldo insuficiente, necessário 1e50c para realizar a chamada"')
+
+                    elif numero[:1] == '2': 
+                        if len(numero) == 9:   
+                            if saldo>25:
+                                saldo-=25
+                                stack.append('T')
+                                print('maq: "saldo %s"' % intToStringCoin(saldo))
+
+                            else:    
+                                print('maq: "Saldo insuficiente, necessário 25c para realizar a chamada"')
+                        else:
+                            print('maq: "Número incorreto, disque novamente um número com 9 digitos"')
+
+                    elif numero[:2] == '80':
+                        if len(numero) == 9:   
+                            if numero[:3] == '800':
+                                stack.append('T')
+                                print('maq: "saldo %s"' % intToStringCoin(saldo))
+                            elif numero[:3] == '808':
+                                if saldo>10:
+                                    saldo-=10
+                                    stack.append('T')
+                                    print('maq: "saldo %s"' % intToStringCoin(saldo))
+                                else:    
+                                    print('maq: "Saldo insuficiente, necessário 10c para realizar a chamada"')
+                        else:
+                            print('maq: "Número incorreto, disque novamente um número com 9 digitos"')
+                    else:
+                        saldo-=15
+                        print('maq: "saldo %s"' % intToStringCoin(saldo))
+        elif linha.strip().upper() == 'POUSAR' or linha.strip().upper() == 'ABORTAR':
+            if len(stack)>0 and stack[-1] == 'T' or  stack[-1] == 'MOEDA':
+                stack = []
+                saldo_string = intToStringCoin(saldo)
+                print('maq: "troco= %s; Volte sempre!"' % saldo_string)
+            else:
+                stack = []
+                break
         else:
-            print('maq: "'+ str(invalidas) +' moeda inválida, saldo = '+total_string+'"')
-        stack.append('MOEDA')
-
-    elif stack[-1] == 'MOEDA' and numero_exp.match(linha.strip()):
-        numero = numero_exp.match(linha).group('numero')
-        if numero[:3] == '601' or numero[3:] == '641':
-            print('maq: "Esse número não é permitido neste telefone. Queira disque um novo número"')
-        elif numero[:2] == '00':
-            if saldo>150:
-                saldo-=150
-                stack.append('T')
-            else:    
-                print('maq: "Saldo insuficiente, necessário 1e50c para realizar a chamada"')
-
-        elif numero[:1] == '2':
-            if saldo>25:
-                saldo-=25
-                stack.append('T')
-            else:    
-                print('maq: "Saldo insuficiente, necessário 25c para realizar a chamada"')
-        elif numero[:2] == '80':
-            if numero[:3] == '800':
-                stack.append('T')
-            elif numero[:3] == '808':
-                if saldo>10:
-                    saldo-=10
-                    stack.append('T')
-                else:    
-                    print('maq: "Saldo insuficiente, necessário 10c para realizar a chamada"')
-
-    elif linha.strip().upper() == 'POUSAR' or linha.strip().upper() == 'ABORTAR':
-        if stack[-1] == 'T' or  stack[-1] == 'MOEDA':
-            saldo_string = intToStringCoin(saldo)
-            print('maq: "troco= %s; Volte sempre!"' % saldo_string)
-            pass
-        else:
-            break
-    else:
-        print('maq: "Não é possível realizar a chamada"')
-    print(stack)
-        
-
+            print('maq: "Não é possível realizar a chamada"')
+            
+run()
